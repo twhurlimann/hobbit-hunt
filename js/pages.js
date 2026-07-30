77d6bdf4d3dff0fa98edca9bc1bf6faae5bee31a
Tyler Hurlimann <twhurlimann@gmail.com>
2026-07-30 09:49:16 -0700

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -302,7 +302,9 @@ The Cursed Ring has power over one of the elements
                 "dragon's fang",
                 "a dragon's fang",
                 "the fang of a dragon",
-                "fang of a dragon"
+                "fang of a dragon",
+                "dragons fang",
+                "a dragons fang"
             ]
 
         },
